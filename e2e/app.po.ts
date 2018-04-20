04712a03f02e05b91598d61986d4663ec1e2ac1c
ajay14312 <ajaynitm10@gmail.com>
2018-04-20 18:19:03 +0530

    Missed files added

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,18 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-weather h2')).getText();
+  }
+
+  getPlaceHolder() {
+    return element(by.css('[type="button"]'));
+  }
+
+  setInputValue() {
+    return element(by.css('[type="text"]')).sendKeys('London,UK');
+  }
+
+  getTrElementData() {
+    return element.all(by.tagName('td')).getText();
   }
 }
